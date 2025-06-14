6b1f448a275684c520e64236a1caad58fd585309
struv <wstruve2@gmail.com>
2025-06-13 17:58:44 -0700

    Debug

--- next.config.js
+++ next.config.js
@@ -2,6 +2,13 @@
 const nextConfig = {
   output: 'standalone',
   reactStrictMode: true,
+  webpack: (config) => {
+    config.module.rules.push({
+      test: /\.css$/,
+      use: ['style-loader', 'css-loader'],
+    });
+    return config;
+  },
 }
 
 module.exports = nextConfig 
